d3dd3227d9be453d71df8eeec111aa0aca656c96
mayconbalves <mayconmustaine@hotmail.com>
2021-11-09 22:00:04 -0300

    chore: ignore build folder

--- build/index.js
+++ build/index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 module.exports = cpfNumber => {
   return cpfNumber.replace(/\D/g, '').replace(/(\d{3})(\d)/, '$1.$2').replace(/(\d{3})(\d)/, '$1.$2').replace(/(\d{3})(\d{1,2})/, '$1-$2').replace(/(-\d{2})\d+?$/, '$1');
 };
--- build/index.test.js
+++ build/index.test.js
@@ -1,2 +1,7 @@
-import cpfMask from '.';
-test('format cpf', () => expect(cpfMask('11111111111')).toBe('111.111.111-11'));
+"use strict";
+
+var _ = _interopRequireDefault(require("."));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+test('format cpf', () => expect((0, _.default)('11111111111')).toBe('111.111.111-11'));
