03b77f678e44b68bb7a1accc7d98a5003da9aaad
mayconbalves <mayconmustaine@hotmail.com>
2021-11-10 22:00:53 -0300

    test: create new cases for cpf mask

--- src/cpf-mask.test.js
+++ src/cpf-mask.test.js
@@ -1,6 +1,13 @@
 import cpfMask from './cpf-mask'
 
 test('format cpf', () => (
-	expect(cpfMask('11111111111'))
-	.toBe('111.111.111-11')
+	expect(cpfMask('11111111111')).toBe('111.111.111-11')
+))
+
+test('not receive letters', () => (
+	expect(cpfMask('abcdefg')).toBe('')
+))
+
+test('should be format correctly cpf number', () => (
+	expect(cpfMask('11111111111')).not.toBe('111.111.11111')
 ))
