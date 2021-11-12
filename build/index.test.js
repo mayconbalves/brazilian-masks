b93fa70921b632b214b3ef2d703417ea5aad427b
mayconbalves <mayconmustaine@hotmail.com>
2021-11-11 21:33:10 -0300

    refactor: removed build folder

--- build/index.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _ = _interopRequireDefault(require("."));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-test('format cpf', () => expect((0, _.default)('11111111111')).toBe('111.111.111-11'));
