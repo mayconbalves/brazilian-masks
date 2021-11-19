24d49e9b35666546226c0089a69d9c8b8b6e01c1
mayconbalves <mayconmustaine@hotmail.com>
2021-11-18 21:39:46 -0300

    feat: create rg mask

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports.cpfMask = require('./cpf')
 module.exports.cnpjMask = require('./cnpj')
 module.exports.cepMask = require('./zipcode')
+module.exports.rgMask = require('./rg')
---
+++ src/rg.js
@@ -0,0 +1,7 @@
+module.exports = (value) =>
+  value
+    .replace(/\D/g, '')
+    .replace(/(\d{2})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+    .replace(/(-\d{1})\d+?$/, '$1')
